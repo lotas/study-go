c565407021635f02791a12b672a4a09cdbd3b554
Yaraslau Kurmyza <lotask@gmail.com>
2017-10-03 01:04:57 +0200

    Added API endpoints for blockchain

--- playground/gochain/blockchain.go
+++ playground/gochain/blockchain.go
@@ -1,4 +1,4 @@
-package blockchain
+package main
 
 import (
 	"crypto/sha256"
@@ -33,12 +33,12 @@ type Blockchain struct {
 	lastBlock           *Block
 }
 
-func NewBlockchain() *Blockchain {
-	bc := &Blockchain{}
+func NewBlockchain() Blockchain {
+	bc := Blockchain{}
 
 	b := Block{
 		previousHash: "1",
-		proof:        100,
+		proof:        1,
 	}
 	bc.chain = append(bc.chain, b)
 	bc.lastBlock = &b
@@ -47,11 +47,9 @@ func NewBlockchain() *Blockchain {
 }
 
 // NewBlock creates new block
-func (bc *Blockchain) NewBlock(proof Proof, previousHash string) *Block {
-	prevHash := previousHash
-	if prevHash == "" {
-		prevHash = Hash(bc.lastBlock)
-	}
+func (bc *Blockchain) NewBlock(proof Proof) *Block {
+	previousHash := Hash(bc.lastBlock)
+
 	b := Block{
 		index:         BlockIndex(len(bc.chain) + 1),
 		timestamp:     time.Now().UnixNano(),
@@ -76,6 +74,21 @@ func (bc *Blockchain) NewTransaction(s Sender, r Recipient, a Amount) BlockIndex
 	})
 
 	return bc.lastBlock.index + 1
+}
+
+// ProofOfWork finds a number p' such that hash(pp') contains leading 4 zeroes
+func ProofOfWork(lastProof Proof) Proof {
+	var proof Proof
+	for validProof(lastProof, proof) == false {
+		proof++
+	}
+	return proof
+}
+
+func validProof(lastProof, proof Proof) bool {
+	str := fmt.Sprintf("%d%d", lastProof, proof)
+	guess := fmt.Sprintf("%x", sha256.Sum256([]byte(str)))
+	return guess[:4] == "0000"
 }
 
 func Hash(block *Block) string {
--- playground/gochain/main.go
+++ playground/gochain/main.go
@@ -2,18 +2,79 @@ package main
 
 import (
 	"fmt"
-
-	"./blockchain"
+	"log"
+	"net/http"
+	"strconv"
 )
 
+const port = 3377
+
+var blockchain Blockchain
+
+func init() {
+	blockchain = NewBlockchain()
+}
+
 func main() {
-	c := blockchain.NewBlockchain()
 
-	c.NewTransaction("me", "you", 999)
-	c.NewTransaction("me2", "you2", 111)
-	c.NewTransaction("me3", "you3", 222)
-	b := c.NewBlock(2, "")
+	// c.NewTransaction("me", "you", 999)
+	// c.NewTransaction("me2", "you2", 111)
+	// c.NewTransaction("me3", "you3", 222)
+	// b := c.NewBlock(2, "")
 
-	fmt.Printf("New blockchain %v\n", c)
-	fmt.Printf("Last block index %v\n", b)
+	// fmt.Printf("New blockchain %v\n", c)
+	// fmt.Printf("Last block index %v\n", b)
+
+	// fmt.Printf("Valid proof for 1: %d\n", ProofOfWork(1))
+
+	http.HandleFunc("/mine", handleMine)
+	http.HandleFunc("/transactions/new", handleTransactionsNew)
+	http.HandleFunc("/chain", handleChain)
+
+	fmt.Printf("Listening on localhost:%d\n", port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf("localhost:%d", port), nil))
+}
+
+func handleMine(w http.ResponseWriter, r *http.Request) {
+	// 1. Calculate next proof
+	lastBlock := blockchain.lastBlock
+	lastProof := lastBlock.proof
+	newProof := ProofOfWork(lastProof)
+
+	// 2. Receive a reward for mining
+	blockchain.NewTransaction("0", "me", 1)
+
+	// 3. Forge new block by adding it to the chain
+	block := blockchain.NewBlock(newProof)
+
+	fmt.Fprintf(w,
+		"Mined new block #%d\ntransactions: %v\nproof: %d\nprevHash: %s\n",
+		block.index,
+		block.transacations,
+		block.proof,
+		block.previousHash,
+	)
+}
+
+func handleTransactionsNew(w http.ResponseWriter, r *http.Request) {
+	sender := r.FormValue("sender")
+	recipient := r.FormValue("recipient")
+	amount, _ := strconv.Atoi(r.FormValue("amount"))
+
+	if sender == "" || recipient == "" || amount == 0 {
+		fmt.Fprintf(w, "Invalid request")
+		return
+	}
+
+	index := blockchain.NewTransaction(Sender(sender), Recipient(recipient), Amount(amount))
+
+	fmt.Fprintf(w, "Transaction will be added to Block %d\n", index)
+}
+
+func handleChain(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Len: %d\nChain: %v\nUnconfirmed: %v\n",
+		len(blockchain.chain),
+		blockchain.chain,
+		blockchain.currentTransactions,
+	)
 }
